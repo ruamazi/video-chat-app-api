b0e1e00b5969f28347a23fb055d47042745eb155
. <barcalover016@gmail.com>
2025-05-08 20:15:27 +0200

    removing console log

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,6 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protectRoute = async (req, res, next) => {
- console.log(req.cookies);
-
  const token = req.cookies.videoCallChatToken;
  if (!token) {
   return res.status(401).json({ error: "Not authorized, no token" });
