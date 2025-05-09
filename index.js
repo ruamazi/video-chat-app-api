b99c5ca5914c0938a7e998a7fc0c607d29347e09
. <barcalover016@gmail.com>
2025-05-09 17:10:26 +0200

    fixing connect to db

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ let isConnected;
 const connectToDB = async () => {
  if (isConnected) return;
  try {
-  const db = await mongoose.connect(process.env.MONGO_URI, {
-   dbName: "your-db-name",
-  });
+  const db = await mongoose.connect(process.env.MONGO_URI);
   isConnected = db.connections[0].readyState;
   console.log("MongoDB connected");
  } catch (err) {
